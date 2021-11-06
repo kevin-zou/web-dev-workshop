f234759d1ef9eb37cb59e1b150b72003082bccd1
Kevin Zou <kevin.zp.zou@gmail.com>
2021-11-06 00:18:33 -0700

    I give up

--- util/firebase.js
+++ util/firebase.js
@@ -8,13 +8,13 @@ import { getFirestore, collection, getDocs } from 'firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDLy_yc64qUFvByO8uO-w0cHifbbAUW4w0",
+  authDomain: "hackcamp-cafe.firebaseapp.com",
+  projectId: "hackcamp-cafe",
+  storageBucket: "hackcamp-cafe.appspot.com",
+  messagingSenderId: "566287504596",
+  appId: "1:566287504596:web:dc13ca6d2d2be6a9c85741",
+  measurementId: "G-2530P5DJMY"
 };
 
 // Initialize Firebase
